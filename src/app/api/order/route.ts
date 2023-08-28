58bf97ae6c145561b029d45c799cb74bfe3b4cd7
lolokos86 <lolokos1986@gmail.com>
2023-08-28 22:54:06 +0300

    modified:   src/app/api/order/route.ts

--- src/app/api/order/route.ts
+++ src/app/api/order/route.ts
@@ -1,18 +1,18 @@
-// import { prisma } from '@/lib/db/prisma';
-// import { NextResponse } from 'next/server';
+import { prisma } from '@/lib/db/prisma';
+import { NextResponse } from 'next/server';
 
-// export const POST = async (req: Request) => {
-//   const body = await req.json();
-//   console.log(body)
-//   if (!body) {
-//     throw new Error('Missing required fields');
-//   }
-//   try {
-//     const newOrder = await prisma.order.create({
+export const POST = async (req: Request) => {
+  const body = await req.json();
+  console.log(body)
+  if (!body) {
+    throw new Error('Missing required fields');
+  }
+  try {
+    // const newOrder = await prisma.order.create({
     
-//     });
-//     return NextResponse.json(newOrder, { status: 201 });
-//   } catch (error) {
-//     console.log(error, `CREATE_ORDER-ERROR`);
-//   }
-// };
+    // });
+    return NextResponse.json('newOrder', { status: 201 });
+  } catch (error) {
+    console.log(error, `CREATE_ORDER-ERROR`);
+  }
+};
