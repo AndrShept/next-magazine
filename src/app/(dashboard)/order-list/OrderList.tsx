a7c4d3665b627650e05bbdb84e1985f4875af831
lolokos86 <lolokos1986@gmail.com>
2023-08-31 18:07:38 +0300

    Changes to be committed: 	modified:   prisma/schema.prisma 	modified:   src/app/(dashboard)/order-list/OrderList.tsx 	modified:   src/app/(dashboard)/order-list/page.tsx

--- src/app/(dashboard)/order-list/OrderList.tsx
+++ src/app/(dashboard)/order-list/OrderList.tsx
@@ -10,42 +10,41 @@ import { OrderListProps } from './page';
 import Image from 'next/image';
 import { formatPrice } from '@/lib/format';
 import { DeleteIcon } from './DeleteIcon';
+import { Separator } from '@/components/ui/separator';
 
 export const OrderList = ({ orders }: { orders: OrderListProps[] }) => {
   return (
     <div>
       <Accordion type='single' collapsible>
-
         {orders.map((order, idx) => (
           <AccordionItem
-            className='border border-zinc-600 rounded-xl px-3 py-2 mt-2 hover:shadow-md'
+            className='border bg-base-100 border-zinc-300 rounded-xl px-3 py-2 mt-2 hover:shadow-md'
             key={order.id}
             value={`item-${idx + 1}`}
           >
             <AccordionTrigger>
               <span>{order.name}</span>
-              <span className='text-sm text-pink-500'>{order.phoneNumber}</span>
+              <span className='text-sm '>{order.phoneNumber}</span>
 
               <span className='text-sm '>{format(order.createdAt)}</span>
             </AccordionTrigger>
             <div className='flex justify-between items-center'>
               <span className='text-sm text-zinc-500'>{order.email}</span>
               <div className='flex items-center gap-1'>
-              <span className='text-sm text-pink-500'>
-                Загальна сума {formatPrice(order.subtotal)}
-              </span>
-              <DeleteIcon orderId={order.id} />
-
+                <span className='text-sm text-pink-500'>
+                  Загальна сума {formatPrice(order.subtotal)}
+                </span>
+                <DeleteIcon orderId={order.id} />
               </div>
-     
-              
             </div>
-            {/* <Separator /> */}
+            <Separator className='mt-4' />
             {order.orderItem.map((item) => (
               <AccordionContent className='mt-2 mb-0' key={item.id}>
                 <div className='grid grid-cols-4 items-center justify-between bg-base-100 px-4 py-2 rounded-md '>
-                  <span>{item.productName}</span>
-                  <div className='relative h-14 w-20'>
+                  <span className='font-medium text-zinc-500'>
+                    {item.productName}
+                  </span>
+                  <div className='relative sm:h-20 h-14 max-w-md'>
                     <Image
                       src={item.imageUrl}
                       className='object-cover '
@@ -53,8 +52,10 @@ export const OrderList = ({ orders }: { orders: OrderListProps[] }) => {
                       fill
                     />
                   </div>
-                  <span>{item.quantity}шт.</span>
-                  <span>{formatPrice(item.productPrice * item.quantity)}</span>
+                  <span className='ml-4'>{item.quantity}шт.</span>
+                  <span className='text-pink-500'>
+                    {formatPrice(item.productPrice * item.quantity)}
+                  </span>
                 </div>
               </AccordionContent>
             ))}
--- src/app/(dashboard)/order-list/page.tsx
+++ src/app/(dashboard)/order-list/page.tsx
@@ -3,18 +3,23 @@ import { prisma } from '@/lib/db/prisma';
 import React from 'react';
 import { OrderList } from './OrderList';
 import { Order, OrderItem } from '@prisma/client';
+import { Divide } from 'lucide-react';
 
 export type OrderListProps = Order & { orderItem: OrderItem[] };
 
 const page = async () => {
-  const orders:OrderListProps[] = await prisma.order.findMany({
-    include:{orderItem:true},
+  const orders: OrderListProps[] = await prisma.order.findMany({
+    include: { orderItem: true },
     orderBy: { createdAt: 'desc' },
   });
+  if (!orders || orders === undefined || orders.length === 0) {
+    return (
+      <div className='text-center text-4xl text-gray-500'>Замовлень немає</div>
+    );
+  }
   return (
     <div className='h-full max-w-3xl mx-auto '>
-
-<OrderList orders={orders}/>
+      <OrderList orders={orders} />
     </div>
   );
 };
