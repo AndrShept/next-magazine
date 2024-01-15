07e979f280f38dbfc05c272b6e68cda0684596af
lolokos86 <lolokos1986@gmail.com>
2024-01-15 14:33:01 +0200

    Changes to be committed: 	modified:   prisma/schema.prisma 	modified:   src/app/api/rating/route.ts

--- src/app/api/rating/route.ts
+++ src/app/api/rating/route.ts
@@ -26,16 +26,27 @@ export const POST = async (req: Request) => {
     const newRating = await prisma.rating.create({
       data: { productId, rating, userId },
     });
-    const ratingArr = await prisma.rating.findMany();
-    const newRatingValue = ratingArr.reduce(
+    const product = await prisma.product.findFirst({
+      where: { id: productId },
+      include: { rating: true },
+    });
+    const newRatingValue = product?.rating.reduce(
       (acc, item) => acc + item.rating,
       0
     );
+    if (!product || !newRatingValue) {
+      return NextResponse.json(
+        { message: 'product not found' },
+        { status: 401 }
+      );
+    }
     console.log(newRatingValue);
     await prisma.product.update({
       where: { id: productId },
       data: {
-        ratingValue: Number((newRatingValue / ratingArr.length).toFixed(1)),
+        ratingValue: Number(
+          (newRatingValue / product?.rating.length).toFixed(1)
+        ),
       },
     });
     return NextResponse.json(newRating, { status: 201 });
